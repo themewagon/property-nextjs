699196de922fecd989d5542867ec497b832dfc22
KhanAnupamShafi <deep71.bd@gmail.com>
2025-11-12 19:34:40 +0600

    next.config updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,20 @@
 /** @type {import('next').NextConfig} */
+const basePath = process.env.NODE_ENV === 'production' ? '/property-nextjs' : '';
+
 const nextConfig = {
   output: 'export',
+  basePath,
+  assetPrefix: basePath,
   images: {
     unoptimized: true,
   },
   trailingSlash: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  env: {
+    NEXT_PUBLIC_BASE_PATH: basePath,
+  },
 };
 
 export default nextConfig;
